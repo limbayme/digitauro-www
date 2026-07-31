248429312c89d280e4a7560a164f5fd3467c72b7
limbay <limbay@LimbayMac.local>
2026-07-31 21:30:56 +0800

    Serve Baidu verification route

---
+++ app/baidu_verify_codeva-9y5PzctR4b.html/route.ts
@@ -0,0 +1,9 @@
+export const dynamic = "force-static";
+
+export function GET() {
+  return new Response("36a581a72083536c68d01bf406792195", {
+    headers: {
+      "content-type": "text/html; charset=utf-8"
+    }
+  });
+}
